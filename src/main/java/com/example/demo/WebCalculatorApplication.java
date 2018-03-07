36d3a8c917640c054fa87f4837d4d68fffe9b8e0
Nagabhushanam <nagabhushanamn@gmail.com>
2018-03-07 09:55:17 +0530

    commit on new changes

--- src/main/java/com/example/demo/WebCalculatorApplication.java
+++ src/main/java/com/example/demo/WebCalculatorApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// this is main file
+
 @SpringBootApplication
 public class WebCalculatorApplication {
 
