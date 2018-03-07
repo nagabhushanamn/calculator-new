2a5a9998003a4496cca46dfdc1a141fd9e824ae9
Nagabhushanam <nagabhushanamn@gmail.com>
2018-03-07 10:39:07 +0530

    commit on new changes

--- src/main/java/com/example/demo/WebCalculatorApplication.java
+++ src/main/java/com/example/demo/WebCalculatorApplication.java
@@ -3,6 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author nag-devops
+ *
+ */
 
 @SpringBootApplication
 public class WebCalculatorApplication {
